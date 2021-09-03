964a58d110dfc45f3431e04ae038710ff25b18f9
ansango <anibalsantosgo@gmail.com>
2021-09-03 13:40:29 +0200

    bands

---
+++ server/models/band-list.js
@@ -0,0 +1,44 @@
+const Band = require("./band");
+
+class BandList {
+  constructor() {
+    this.bands = [
+      new Band("Ramones"),
+      new Band("Black Flag"),
+      new Band("Ceremony"),
+      new Band("Turnstile"),
+    ];
+  }
+
+  addBand(name) {
+    const newBand = new Band(name);
+    this.bands.push(newBand);
+    return this.bands;
+  }
+
+  removeRand(id) {
+    this.bands = this.bands.filter((band) => banmd.id !== id);
+  }
+
+  getBands() {
+    return this.bands;
+  }
+
+  increaseVotes(id) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) {
+        band.votes += 1;
+      }
+      return band;
+    });
+  }
+
+  changeBandName(id, newName) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) {
+        band.name = newName;
+      }
+      return band;
+    });
+  }
+}

86ed4281ed979bf54d1b2ab6bbb0896ab0a4ba5a
ansango <anibalsantosgo@gmail.com>
2021-09-03 18:16:33 +0200

    chart

--- client/src/components/BandChart.js
+++ client/src/components/BandChart.js
@@ -14,19 +14,36 @@ export const BandChart = () => {
   useEffect(() => {
     const ctx = document.getElementById("myChart");
     const chart = new Chart(ctx, {
-      type: "doughnut",
+      type: "bar",
       data: {
         labels: bands.map((band) => band.name),
         datasets: [
           {
             label: "# of Votes",
             data: bands.map((band) => band.votes),
-            backgroundColor: bands.map((band) => band.color),
-            hoverOffset: 4,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(201, 203, 207, 0.2)",
+            ],
+            borderColor: [
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(153, 102, 255)",
+              "rgb(201, 203, 207)",
+            ],
+            borderWidth: 1,
           },
         ],
       },
-      options: { animation: false },
+      options: { indexAxis: "y" },
     });
     return () => {
       chart.destroy();
--- server/models/band.js
+++ server/models/band.js
@@ -1,14 +1,10 @@
 const { v4: uuidv4 } = require("uuid");
 
-const colorRandom = () =>
-  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-
 class Band {
   constructor(name) {
     this.id = uuidv4();
     this.name = name;
     this.votes = 0;
-    this.color = colorRandom();
   }
 }
 
